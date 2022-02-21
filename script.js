00a132816533e408b2a69b04de1a2d86c2827652
Nash3112 <cesarestefanoperezpalomino@gmail.com>
2022-02-20 23:42:44 -0500

    main

---
+++ script.js
@@ -0,0 +1,17 @@
+//crear una función para aparecer modal de contáctanos.  (contactus())
+
+function contactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "show_modal";
+   }
+   
+   //crear una función para desaparecer el modal de contáctanos (closecontactus())
+   
+   function closecontactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "closemodal";
+   }
+   
+   //crear el modal del login-session
+   
+   //crear el modal de shopping
